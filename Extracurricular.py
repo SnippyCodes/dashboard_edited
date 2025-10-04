60cd437123c2695a8365e5126345e517b06bdfb3
Snipp748 <noobadgaming748@gmail.com>
2025-10-04 22:42:53 +0530

    Added Logo of Clubs

---
+++ Extracurricular.py
@@ -0,0 +1,9 @@
+from PIL import Image as p
+antariksh_logo =  p.open("Antariksh.jpg")
+drone_logo = p.open("Drone.png")
+srewdrivers_logo = p.open("Srewdrivers.png")
+Iot_logo = p.open("IOT.png")
+Robotics_Logo = p.open("Robotics.png")
+Speak = p.open("Speak.png")
+
+antariksh_recruitment = "https://forms.gle/3SDooPE7zkH8T2mVA"
